6055c2e167dd542490ef955ab06a0e2db69f6565
Anders Liden <devel@liden.cx>
2019-04-23 06:59:22 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace RPG_Dice_Roller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fourMaximumTextBox = new System.Windows.Forms.TextBox();
+            this.fourEnabledCheckBox = new System.Windows.Forms.CheckBox();
+            this.fourNumberOfDiceTextBox = new System.Windows.Forms.TextBox();
+            this.dice = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fourAdditionPerDieTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fourCompleteResultTextBox = new System.Windows.Forms.TextBox();
+            this.fourResultTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rollDieButton = new System.Windows.Forms.Button();
+            this.sixMaximumTextBox = new System.Windows.Forms.TextBox();
+            this.sixNumberOfDiceTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sixAdditionPerDieTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.sixCompleteResultTextBox = new System.Windows.Forms.TextBox();
+            this.sixEnabledCheckBox = new System.Windows.Forms.CheckBox();
+            this.sixResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fourMaximumTextBox
+            // 
+            this.fourMaximumTextBox.Location = new System.Drawing.Point(91, 130);
+            this.fourMaximumTextBox.Name = "fourMaximumTextBox";
+            this.fourMaximumTextBox.Size = new System.Drawing.Size(119, 31);
+            this.fourMaximumTextBox.TabIndex = 0;
+            this.fourMaximumTextBox.Text = "4";
+            this.fourMaximumTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // fourEnabledCheckBox
+            // 
+            this.fourEnabledCheckBox.AutoSize = true;
+            this.fourEnabledCheckBox.Location = new System.Drawing.Point(39, 130);
+            this.fourEnabledCheckBox.Name = "fourEnabledCheckBox";
+            this.fourEnabledCheckBox.Size = new System.Drawing.Size(28, 27);
+            this.fourEnabledCheckBox.TabIndex = 1;
+            this.fourEnabledCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // fourNumberOfDiceTextBox
+            // 
+            this.fourNumberOfDiceTextBox.Location = new System.Drawing.Point(240, 130);
+            this.fourNumberOfDiceTextBox.Name = "fourNumberOfDiceTextBox";
+            this.fourNumberOfDiceTextBox.Size = new System.Drawing.Size(70, 31);
+            this.fourNumberOfDiceTextBox.TabIndex = 2;
+            this.fourNumberOfDiceTextBox.Text = "1";
+            this.fourNumberOfDiceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dice
+            // 
+            this.dice.AutoSize = true;
+            this.dice.Location = new System.Drawing.Point(316, 133);
+            this.dice.Name = "dice";
+            this.dice.Size = new System.Drawing.Size(52, 25);
+            this.dice.TabIndex = 4;
+            this.dice.Text = "dice";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "+";
+            // 
+            // fourAdditionPerDieTextBox
+            // 
+            this.fourAdditionPerDieTextBox.Location = new System.Drawing.Point(404, 130);
+            this.fourAdditionPerDieTextBox.Name = "fourAdditionPerDieTextBox";
+            this.fourAdditionPerDieTextBox.Size = new System.Drawing.Size(70, 31);
+            this.fourAdditionPerDieTextBox.TabIndex = 6;
+            this.fourAdditionPerDieTextBox.Text = "0";
+            this.fourAdditionPerDieTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(480, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "per die";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(157, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Max";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Number";
+            // 
+            // fourCompleteResultTextBox
+            // 
+            this.fourCompleteResultTextBox.Location = new System.Drawing.Point(575, 127);
+            this.fourCompleteResultTextBox.Name = "fourCompleteResultTextBox";
+            this.fourCompleteResultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.fourCompleteResultTextBox.Size = new System.Drawing.Size(573, 31);
+            this.fourCompleteResultTextBox.TabIndex = 10;
+            this.fourCompleteResultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // fourResultTextBox
+            // 
+            this.fourResultTextBox.Location = new System.Drawing.Point(1192, 130);
+            this.fourResultTextBox.Name = "fourResultTextBox";
+            this.fourResultTextBox.Size = new System.Drawing.Size(100, 31);
+            this.fourResultTextBox.TabIndex = 11;
+            this.fourResultTextBox.Text = "1";
+            this.fourResultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1232, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Total";
+            // 
+            // rollDieButton
+            // 
+            this.rollDieButton.Location = new System.Drawing.Point(1146, 1137);
+            this.rollDieButton.Name = "rollDieButton";
+            this.rollDieButton.Size = new System.Drawing.Size(146, 52);
+            this.rollDieButton.TabIndex = 13;
+            this.rollDieButton.Text = "Roll Dice";
+            this.rollDieButton.UseVisualStyleBackColor = true;
+            // 
+            // sixMaximumTextBox
+            // 
+            this.sixMaximumTextBox.Location = new System.Drawing.Point(1192, 188);
+            this.sixMaximumTextBox.Name = "sixMaximumTextBox";
+            this.sixMaximumTextBox.Size = new System.Drawing.Size(100, 31);
+            this.sixMaximumTextBox.TabIndex = 22;
+            this.sixMaximumTextBox.Text = "1";
+            this.sixMaximumTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sixNumberOfDiceTextBox
+            // 
+            this.sixNumberOfDiceTextBox.Location = new System.Drawing.Point(575, 185);
+            this.sixNumberOfDiceTextBox.Name = "sixNumberOfDiceTextBox";
+            this.sixNumberOfDiceTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.sixNumberOfDiceTextBox.Size = new System.Drawing.Size(573, 31);
+            this.sixNumberOfDiceTextBox.TabIndex = 21;
+            this.sixNumberOfDiceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(480, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "per die";
+            // 
+            // sixAdditionPerDieTextBox
+            // 
+            this.sixAdditionPerDieTextBox.Location = new System.Drawing.Point(404, 188);
+            this.sixAdditionPerDieTextBox.Name = "sixAdditionPerDieTextBox";
+            this.sixAdditionPerDieTextBox.Size = new System.Drawing.Size(70, 31);
+            this.sixAdditionPerDieTextBox.TabIndex = 19;
+            this.sixAdditionPerDieTextBox.Text = "0";
+            this.sixAdditionPerDieTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(374, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 25);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "+";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(316, 191);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "dice";
+            // 
+            // sixCompleteResultTextBox
+            // 
+            this.sixCompleteResultTextBox.Location = new System.Drawing.Point(240, 188);
+            this.sixCompleteResultTextBox.Name = "sixCompleteResultTextBox";
+            this.sixCompleteResultTextBox.Size = new System.Drawing.Size(70, 31);
+            this.sixCompleteResultTextBox.TabIndex = 16;
+            this.sixCompleteResultTextBox.Text = "1";
+            this.sixCompleteResultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sixEnabledCheckBox
+            // 
+            this.sixEnabledCheckBox.AutoSize = true;
+            this.sixEnabledCheckBox.Location = new System.Drawing.Point(39, 188);
+            this.sixEnabledCheckBox.Name = "sixEnabledCheckBox";
+            this.sixEnabledCheckBox.Size = new System.Drawing.Size(28, 27);
+            this.sixEnabledCheckBox.TabIndex = 15;
+            this.sixEnabledCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // sixResultTextBox
+            // 
+            this.sixResultTextBox.Location = new System.Drawing.Point(91, 188);
+            this.sixResultTextBox.Name = "sixResultTextBox";
+            this.sixResultTextBox.Size = new System.Drawing.Size(119, 31);
+            this.sixResultTextBox.TabIndex = 14;
+            this.sixResultTextBox.Text = "4";
+            this.sixResultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 1229);
+            this.Controls.Add(this.sixMaximumTextBox);
+            this.Controls.Add(this.sixNumberOfDiceTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.sixAdditionPerDieTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.sixCompleteResultTextBox);
+            this.Controls.Add(this.sixEnabledCheckBox);
+            this.Controls.Add(this.sixResultTextBox);
+            this.Controls.Add(this.rollDieButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fourResultTextBox);
+            this.Controls.Add(this.fourCompleteResultTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fourAdditionPerDieTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dice);
+            this.Controls.Add(this.fourNumberOfDiceTextBox);
+            this.Controls.Add(this.fourEnabledCheckBox);
+            this.Controls.Add(this.fourMaximumTextBox);
+            this.MinimumSize = new System.Drawing.Size(1403, 1300);
+            this.Name = "Form1";
+            this.Text = "Superior Advanced Die Roller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox fourEnabledCheckBox;
+        private System.Windows.Forms.TextBox fourMaximumTextBox;
+        private System.Windows.Forms.TextBox fourNumberOfDiceTextBox;
+        private System.Windows.Forms.TextBox fourAdditionPerDieTextBox;
+        private System.Windows.Forms.TextBox fourCompleteResultTextBox;
+        private System.Windows.Forms.TextBox fourResultTextBox;
+        private System.Windows.Forms.CheckBox sixEnabledCheckBox;
+        private System.Windows.Forms.TextBox sixMaximumTextBox;
+        private System.Windows.Forms.TextBox sixNumberOfDiceTextBox;
+        private System.Windows.Forms.TextBox sixAdditionPerDieTextBox;
+        private System.Windows.Forms.TextBox sixCompleteResultTextBox;
+        private System.Windows.Forms.TextBox sixResultTextBox;
+        private System.Windows.Forms.Button rollDieButton;
+        private System.Windows.Forms.Label dice;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
